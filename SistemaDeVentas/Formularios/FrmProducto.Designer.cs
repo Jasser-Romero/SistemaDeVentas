5027a2cb75592b0140ca8645745fa97be1a4d363
Oscar García <oscargggarcia230105@gmail.com>
2022-06-12 20:57:44 -0600

    Se agregaron los metodos del Producto

--- SistemaDeVentas/Formularios/FrmProducto.Designer.cs
+++ SistemaDeVentas/Formularios/FrmProducto.Designer.cs
@@ -32,8 +32,31 @@
             this.label10 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvproductos = new System.Windows.Forms.DataGridView();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblExistencias = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtExistencias = new System.Windows.Forms.TextBox();
+            this.txtPrecVentas = new System.Windows.Forms.TextBox();
+            this.rtbDescripcion = new System.Windows.Forms.RichTextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.pbProducto = new System.Windows.Forms.PictureBox();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvproductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProducto)).BeginInit();
             this.SuspendLayout();
             // 
             // label10
@@ -41,18 +64,19 @@
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label10.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label10.Location = new System.Drawing.Point(57, 12);
+            this.label10.Location = new System.Drawing.Point(65, 16);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(90, 21);
+            this.label10.Size = new System.Drawing.Size(114, 28);
             this.label10.TabIndex = 27;
             this.label10.Text = "Productos";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1, 40);
+            this.pictureBox2.Location = new System.Drawing.Point(1, 53);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(743, 20);
+            this.pictureBox2.Size = new System.Drawing.Size(849, 27);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 26;
             this.pictureBox2.TabStop = false;
@@ -60,27 +84,233 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(48, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(55, 49);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 25;
             this.pictureBox1.TabStop = false;
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(349, 403);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(94, 29);
+            this.btnAgregar.TabIndex = 28;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(471, 403);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(94, 29);
+            this.btnBorrar.TabIndex = 29;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(594, 403);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(94, 29);
+            this.btnActualizar.TabIndex = 30;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(725, 403);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(94, 29);
+            this.btnLimpiar.TabIndex = 31;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // dgvproductos
+            // 
+            this.dgvproductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvproductos.Location = new System.Drawing.Point(1, 454);
+            this.dgvproductos.Name = "dgvproductos";
+            this.dgvproductos.RowHeadersWidth = 51;
+            this.dgvproductos.RowTemplate.Height = 29;
+            this.dgvproductos.Size = new System.Drawing.Size(849, 204);
+            this.dgvproductos.TabIndex = 32;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(35, 171);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(74, 20);
+            this.lblCategoria.TabIndex = 33;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(35, 249);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(64, 20);
+            this.lblNombre.TabIndex = 34;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(35, 323);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(87, 20);
+            this.lblDescripcion.TabIndex = 35;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(349, 171);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(58, 20);
+            this.lblCodigo.TabIndex = 36;
+            this.lblCodigo.Text = "Codigo";
+            // 
+            // lblExistencias
+            // 
+            this.lblExistencias.AutoSize = true;
+            this.lblExistencias.Location = new System.Drawing.Point(349, 249);
+            this.lblExistencias.Name = "lblExistencias";
+            this.lblExistencias.Size = new System.Drawing.Size(80, 20);
+            this.lblExistencias.TabIndex = 37;
+            this.lblExistencias.Text = "Existencias";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(349, 323);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 20);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "Precio de venta";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(128, 246);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(175, 27);
+            this.txtNombre.TabIndex = 39;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(466, 171);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(175, 27);
+            this.txtCodigo.TabIndex = 40;
+            // 
+            // txtExistencias
+            // 
+            this.txtExistencias.Location = new System.Drawing.Point(466, 249);
+            this.txtExistencias.Name = "txtExistencias";
+            this.txtExistencias.Size = new System.Drawing.Size(175, 27);
+            this.txtExistencias.TabIndex = 41;
+            // 
+            // txtPrecVentas
+            // 
+            this.txtPrecVentas.Location = new System.Drawing.Point(466, 320);
+            this.txtPrecVentas.Name = "txtPrecVentas";
+            this.txtPrecVentas.Size = new System.Drawing.Size(175, 27);
+            this.txtPrecVentas.TabIndex = 42;
+            // 
+            // rtbDescripcion
+            // 
+            this.rtbDescripcion.Location = new System.Drawing.Point(128, 320);
+            this.rtbDescripcion.Name = "rtbDescripcion";
+            this.rtbDescripcion.Size = new System.Drawing.Size(175, 109);
+            this.rtbDescripcion.TabIndex = 43;
+            this.rtbDescripcion.Text = "";
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(128, 168);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(175, 28);
+            this.cmbCategoria.TabIndex = 44;
+            // 
+            // pbProducto
+            // 
+            this.pbProducto.Location = new System.Drawing.Point(667, 102);
+            this.pbProducto.Name = "pbProducto";
+            this.pbProducto.Size = new System.Drawing.Size(173, 178);
+            this.pbProducto.TabIndex = 45;
+            this.pbProducto.TabStop = false;
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(725, 283);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(69, 20);
+            this.lblProducto.TabIndex = 46;
+            this.lblProducto.Text = "Producto";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(83, 102);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(193, 20);
+            this.label8.TabIndex = 47;
+            this.label8.Text = "INFORMACION GENERAL:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(372, 102);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(204, 20);
+            this.label9.TabIndex = 48;
+            this.label9.Text = "INFORMACION DE TIENDA:";
+            // 
             // FrmProducto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(852, 659);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.pbProducto);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.rtbDescripcion);
+            this.Controls.Add(this.txtPrecVentas);
+            this.Controls.Add(this.txtExistencias);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblExistencias);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.dgvproductos);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FrmProducto";
             this.Text = "FrmProducto";
             this.Load += new System.EventHandler(this.FrmProducto_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvproductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProducto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,5 +321,26 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgvproductos;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblExistencias;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtExistencias;
+        private System.Windows.Forms.TextBox txtPrecVentas;
+        private System.Windows.Forms.RichTextBox rtbDescripcion;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.PictureBox pbProducto;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
--- SistemaDeVentas/Formularios/FrmProducto.cs
+++ SistemaDeVentas/Formularios/FrmProducto.cs
@@ -1,4 +1,5 @@
 ﻿using SistemaDeVentas.AppCore.Interfaces;
+using SistemaDeVentas.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,7 @@ namespace SistemaDeVentas.Formularios
     public partial class FrmProducto : Form
     {
         public IProductoService ProductoService { get; set; }
+        int id = 0;
         public FrmProducto()
         {
             InitializeComponent();
@@ -21,7 +23,107 @@ namespace SistemaDeVentas.Formularios
 
         private void FrmProducto_Load(object sender, EventArgs e)
         {
+            dgvproductos.DataSource = null;
+            dgvproductos.DataSource = ProductoService.GetAll();
+        }
 
+
+        private bool Validate()
+        {
+            if (string.IsNullOrEmpty(cmbCategoria.Text) || string.IsNullOrEmpty(txtNombre.Text) ||
+               string.IsNullOrEmpty(rtbDescripcion.Text) || string.IsNullOrEmpty(txtCodigo.Text) ||
+               string.IsNullOrEmpty(txtPrecVentas.Text) || string.IsNullOrEmpty(txtExistencias.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void Clear()
+        {
+            cmbCategoria.Text = "";
+            txtNombre.Text = "";
+            rtbDescripcion.Text = "";
+            txtCodigo.Text = "";
+            txtExistencias.Text = "";
+            txtPrecVentas.Text = "";
+        }
+
+        private void LoadDataGridView()
+        {
+            dgvproductos.DataSource = null;
+            dgvproductos.DataSource = ProductoService.GetAll();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (!Validate())
+            {
+                MessageBox.Show("No puede dejar un espacio en blanco");
+                return;
+            }
+
+            Producto producto = new Producto()
+            {
+                Categoria = cmbCategoria.Text,
+                Nombre = txtNombre.Text,
+                Descripcion = rtbDescripcion.Text,
+                Codigo = txtCodigo.Text,
+                Stock = int.Parse(txtExistencias.Text),
+                PrecioVenta = decimal.Parse(txtPrecVentas.Text)
+            };
+            ProductoService.Create(producto);
+
+            Clear();
+            LoadDataGridView();
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (id == 0)
+            {
+                MessageBox.Show("No se ha seleccionado ningun cliente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (dgvproductos.CurrentRow.Selected == false)
+            {
+                MessageBox.Show("Seleccione un cliente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Producto producto = new Producto()
+            {
+                Id = (int)dgvproductos.CurrentRow.Cells[0].Value,
+                Categoria = cmbCategoria.Text,
+                Nombre = txtNombre.Text,
+                Descripcion = rtbDescripcion.Text,
+                Codigo = txtCodigo.Text,
+                Stock = int.Parse(txtExistencias.Text),
+                PrecioVenta = decimal.Parse(txtPrecVentas.Text)
+            };
+            ProductoService.Update(producto);
+            dgvproductos.DataSource = ProductoService.GetAll();
+            Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvproductos.Rows.GetRowCount(DataGridViewElementStates.Selected) == 0)
+            {
+                MessageBox.Show("Error, seleccione", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int id = (int)dgvproductos.CurrentRow.Cells[0].Value;
+            Producto producto = ProductoService.FindById(id);
+            ProductoService.Delete(producto);
+            LoadDataGridView();
+            Clear();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Clear();
+            id = 0;
         }
     }
 }
